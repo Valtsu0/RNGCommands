a371ab37b3bf6386c2b5273f424ec189a42ddf91
Valtsu0 <Valtsutank@gmail.com>
2024-02-13 13:00:20 +0200

    improved help messages, reject extra arguments

--- src/RNGCommands.cs
+++ src/RNGCommands.cs
@@ -29,6 +29,7 @@ public class RNGCommands
 
         public override Tuple<bool, string> Execute(string[] args)
         {
+            if (args.Length > 0) return new Tuple<bool, string>(false, "Too many arguments!");
             System.Random random = new System.Random();
 
             FeedbackHelper.SendFeedbackMessage(Actions.RandomElement(random));
@@ -51,6 +52,7 @@ public class RNGCommands
 
         public override Tuple<bool, string> Execute(string[] args)
         {
+            if (args.Length > 0) return new Tuple<bool, string>(false, "Too many arguments!");
             System.Random random = new System.Random();
 
             FeedbackHelper.SendFeedbackMessage(Possibilities.RandomElement(random));
@@ -66,18 +68,13 @@ public class RNGCommands
 
     public class RNGCommand : Command, IHelpMessage
     {
-        private static List<string> AdjectiveOne = new List<string>(){ "artless", "bawdy", "beslubbering", "bootless", "churlish", "cockered", "clouted", "craven", "currish","dankish","dissembling","droning","errant","fawning","fobbing","froward","frothy","gleeking","goatish","gorbellied","impertinent","infectious","jarring","joggerheaded","lumpish","mammering","mangled","mewling","paunchy","pribbling","puking","puny","rank","reeky","roguish","ruftish","saucy","spleeny","spongy","surly","tottering","unmuzzled","vain","venomed","villainous","warped","wayward","weedy","yeasty"};
-
-        private static List<string> AdjectiveTwo = new List<string>(){"base-court","bat-forling","beef-witted","beetle-headed","boil-brained","clapper-clawed","clay-brained","common-kissing","crook-pated","dismal-dreaming","dizzy-eyed","doghearted","dread-bolted","earth-vexing","elf-skinned","fat-kidneyed","fen-sucked","flap-mothed","fly-bitten","folly-fallen","fool-born","fill-gorged","guts-griping","half-faced","hasty-witted","hedge-born","hell-hated","idle-headed","ill-breeding","ill-nurtured","knotty-pated","milk-livered","motley-minded","onion-eyed","plume-plucked","pottle-deep","pox-marked","reeling-ripe","rough-hewn","rude-growing","rump-faced","shard-borne","sheep-biting","spur-galled","swag-bellied","tardy-gaited","tickle-brained","toad-spotted","unchin-snoted","weather-bitten"};
-
-        private static List<string> Noun = new List<string>(){"apple-john","baggage","barnacle","bladder","boar-pig","bugbear","bum-bailey","canket-blossom","clack-dish","clotpole","coxcomb","codpiece","death-token","dewberry","flap-dragon","flax-wench","flirt-gill","foot-licker","futilarrian","giglet","gudgeon","haggard","harpy","hedge-pig","horn-beast","hugger-mugger","joithead","lewduster","lout","maggot-pie","malt-worm","mammet","measle","minnow","miscreant","moldwarp","mumble-news","nut-hook","pigeon-egg","pignut","puttock","pumbion","ratsbane","scut","skainsmate","strumpot","varlot","vassal","wheyface","wagtail"};
-
         public RNGCommand(string name, string[] aliases = null, string harmonyId = null) : base(name, aliases, harmonyId)
         {
         }
 
         public override Tuple<bool, string> Execute(string[] args)
         {
+            if (args.Length > 2) return new Tuple<bool, string>(false, "Too many arguments!");
 
             int lower = 1;
             int upper = 15;
@@ -87,7 +84,7 @@ public class RNGCommands
             if (args.Length == 1) {
                 fail = !int.TryParse(args[0], out upper);
                 if (fail) return new Tuple<bool, string>(false, "Unable to parse arguments!");
-            } else if (args.Length > 1)  {
+            } else if (args.Length == 2)  {
                 int arg0;
                 int arg1;
                 fail = !int.TryParse(args[0], out arg0);
@@ -106,7 +103,7 @@ public class RNGCommands
 
         public string GetHelpMessage()
         {
-            return "<b>/rng</b> - by default gives a number between 1 and 15. Upper and lower bounds can be changed by giving arguments.";
+            return "/rng | <b>/rng [upper] | /rng [lower] [upper]</b> - by default gives a number between 1 and 15. Upper and lower bounds can be changed by giving arguments.";
         }
     }
 
@@ -129,7 +126,7 @@ public class RNGCommands
 
         public string GetHelpMessage()
         {
-            return "<b>/choose</b> - chooses a random argument.";
+            return "<b>/choose <argument1> [argument2] ...</b> - chooses a random argument.";
         }
     }
 }
